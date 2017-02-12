3ba49a6b42efd8c7c3bea5c457ec8b1a26f4679d
Bruno Kenji <gilga-mesh@hotmail.com>
2017-02-12 15:22:30 -0200

    implemented function that reads user input and checks if thechar is correct

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -5,17 +5,27 @@
 (defn perdeu [] (println "Voce perdeu"))
 (defn ganhou [] (println "Voce ganhou!"))
 
+(defn le-letra! [] (read-line))
+
 (defn letras-faltantes [palavra acertos]
   (remove (fn [letra] (str/includes? acertos (str letra))) palavra))
 
 (defn acertou-a-palavra-toda? [palavra acertos]
   (empty? (letras-faltantes palavra acertos)))
 
+(defn avalia-chute [chute, vidas, palavra, acertos]
+  (if (acertou? palavra chute)
+      (println "Acertou!"
+        (jogo vidas palavra (conj acertos chute)))
+      (println "Errou!"
+        (jogo (dec vidas) palavra acertos))))
+
 (defn jogo [vidas palavra acertos]
   (if (= vidas 0)
     (perdeu)
     (if (acertou-a-palavra-toda? palavra acertos)
-      (ganhou))))
+      (ganhou)
+      (avalia-chute (le-letra!) vidas palavra acertos))))
 
 (defn -main [& args]
   (jogo 3 "PARALELEPIPEDO" (hash-set)))
